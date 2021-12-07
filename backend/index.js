bb513086acd611e9a9042bb4eca876ba80558bc6
ricpla <ricpla17@gmail.com>
2021-12-07 11:28:41 -0400

    entrar

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const multer = require('multer');
+const path = require('path');
 
 // Initializations
 const app = express();
@@ -9,7 +10,19 @@ const app = express();
 app.set('port', 3000);
 
 // Middlewares
-app.use(morgan());
+app.use(morgan('dev'));
+const storage = multer.diskStorage({
+    destination: path.join(__dirname, 'public/uploads'),
+    filename(req, file, cb){
+        cb(null, new Date().getTime() + path.extname(file.originalname));
+    }
+});
+app.use(multer({storage}).single('image'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+// Routes
+app.use(require('./routes/books'));
 
 // Start the server
 app.listen(app.get('port'), () =>{
